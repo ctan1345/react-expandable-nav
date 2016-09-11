387718fdd5415ed1c0e984f6f490718e2bf32195
Bryce Jacobs <popasquat89@gmail.com>
2016-09-11 12:53:06 -0500

    ReactNavMenu - lazy lookup idx

--- src/components/ExpandableNavMenu.jsx
+++ src/components/ExpandableNavMenu.jsx
@@ -10,26 +10,21 @@ var ExpandableNavMenu = React.createClass({
     fullStyle: React.PropTypes.object,
     smallStyle: React.PropTypes.object
   },
-  getInitialState() {
-    if (!this.props.children) {
-      return {
-        active: 0
-      };
+  calculateActiveIdx() {
+    for(let i = 0; i < this.props.children.length){
+        if(this.props.children[i].props.active){
+          return i
+        }
     }
 
-    for (var i = 0; i < this.props.children.length; i++) {
-      var child = this.props.children[i];
-      if (child.props.active) {
-        return {
-          active: i
-        };
-      }
+    if(this.state.hasOwnProperty('active')){
+        return this.state.active
     }
 
-    return {
-      active: 0
-    };
+    const active = 0;
+    this.setState({active})
 
+    return active
   },
   render() {
     var ulStyle = assign({
@@ -42,13 +37,14 @@ var ExpandableNavMenu = React.createClass({
     var classes = "nav navbar-nav " +
       joinClasses(this.props.className, this.props.expanded ? this.props.fullClass : this.props.smallClass);
 
+    const activeIdx = calculateActiveIdx()
     return (
       <ul className={classes} style={ulStyle}>
         {
             React.Children.map(this.props.children, (child, index) =>{
                 return React.cloneElement(child, {
                     expanded: this.props.expanded,
-                    active: this.state.active === index,
+                    active: activeIdx === index,
                     key: child.key ? child.key : index,
                     ref: child.ref,
                     onSelect: this.handleSelect.bind(null, index)
